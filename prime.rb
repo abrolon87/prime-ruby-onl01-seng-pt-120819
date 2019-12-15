2715407f1d6e4ac23f55ee76d95a3a2601fa5dc1
Amanda Rolon <abrolon87@gmail.com>
2019-12-15 20:45:06 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,17 @@
 # Add  code here!
+# def prime?(num)
+#   if num < 0 or num == 0 or num == 1
+#     return false
+#   else
+#     (2..num-1).to_a.all? do |n|
+#       num % n != 0
+#     end
+#   end
+# end
+
 def prime?(num)
-  if num < 0 or num == 0 or num == 1
-    return false
+  if Prime.prime?(num)
+    return true
   else
-    (2..num-1).to_a.all? do |n|
-      num % n != 0
-    end
-  end
-
-
-
+    return false
 end
